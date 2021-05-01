c61d413bd852525829803631ccc51b608adc6148
Alejandro Rossi <alejandro.rossi@frubana.com>
2021-04-30 21:59:10 -0300

    desafio

--- 04-entrega/begin/entities/player/Player.gd
+++ 04-entrega/begin/entities/player/Player.gd
@@ -2,47 +2,51 @@ extends KinematicBody2D
 
 onready var cannon = $Cannon
 
-export (float) var ACCELERATION:float = 20.0
+export (float) var ACCELERATION:float = 50.0
 export (float) var H_SPEED_LIMIT:float = 600.0
 export (float) var FRICTION_WEIGHT:float = 0.1
-export (float) var gravity:float = 10
+export (float) var gravity: float = 10
 
 var velocity:Vector2 = Vector2.ZERO
 var projectile_container
+var jump_speed:int = 800
+var floorDirection:Vector2 = Vector2.UP
 
 func initialize(projectile_container):
 	self.projectile_container = projectile_container
 	cannon.projectile_container = projectile_container
 
-func _physics_process(delta):
-	
+func get_input():
+
 	# Cannon rotation
 	var mouse_position:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
-	
+
 	# Cannon fire
 	if Input.is_action_just_pressed("fire_cannon"):
 		if projectile_container == null:
 			projectile_container = get_parent()
 			cannon.projectile_container = projectile_container
 		cannon.fire()
-	
-	# Player movement
+
 	var h_movement_direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
+
 	if h_movement_direction != 0:
 		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
 	else:
 		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
-	
-	position += velocity * delta
-	
-#	velocity.y += gravity
-	move_and_slide(velocity * delta)
-	position.y += gravity
-#	position += velocity * delta
+
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y -= jump_speed
 
 
 
 func _ready():
 	add_to_group("player")
+
+
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta * ACCELERATION
+	move_and_slide(velocity * delta * ACCELERATION, floorDirection)
